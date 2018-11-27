f44260784831d1d0f8ca0d5cd9d3e33a7feaa75e
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2018-11-28 00:10:23 +0200

    Replace graphviz with pydot

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, send_file
-from graphviz import Digraph
+import pydot
 
 app = Flask(__name__)
 
@@ -21,19 +21,34 @@ def parse_request():
 
     graph = Digraph('G', filename = f"{name}.gv", format = 'png')
 
+    graphList = pydot.graph_from_dot_file(f"{name}.gv", encoding = 'utf-8')
+    graph = graphList[0]
+    graph.set_strict(True)
+    print(type(graphList))
+    print(type(graph))
+
+    node_app = pydot.Node(name, style = "filled", fillcolor = "#0000ff")
+    graph.add_node(node_app)
     for entry in dependencies:
         print(entry)
         for k, v in entry.items():
             print("key: " + k)
             print("val: " + v)
+            node_shape = ""
             if v == "database":
-                graph.attr('node', shape='box')
+                node_shape = 'box'
                 print("database")
             else:
-                graph.attr('node', shape='ellipse')
-            graph.edge(name, k)
+                node_shape = 'ellipse'
 
-    graph.render()
+            node_svc = pydot.Node(k, style = "filled", fillcolor = "green", shape = node_shape)
+            graph.add_node(node_svc)
+            graph.add_edge(pydot.Edge(node_app, node_svc))
+
+    graph.write_png(f"{name}.png")
+    graph.write(f"{name}.gv")
+
+    return send_file(f"{name}.png", mimetype='image/gif')
 
     return send_file(f"{name}.gv.png", mimetype='image/gif')
 
