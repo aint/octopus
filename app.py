60eaef22745e2c347b368b32fe971992dfde357f
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-04-04 23:37:56 +0300

    Configure server IP for using with docker

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def dependency_type(type):
     }[type.lower()]
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
